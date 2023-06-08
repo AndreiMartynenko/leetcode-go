a50cb3f294585850288ff372a6d1965f64a88ac5
AndrewLondon <andrewmart@mail.com>
2023-06-08 23:36:28 +0100

    Updated

---
+++ explore/arraysandstrings/reverse-string.go
@@ -0,0 +1,16 @@
+package main
+
+func reverseString(s []byte) {
+	leftCharacter := 0
+	rightCharacter := len(s) - 1
+	for leftCharacter < rightCharacter {
+		// Swap the characters at leftCharacter and rightCharacter positions
+		s[leftCharacter], s[rightCharacter] = s[rightCharacter], s[leftCharacter]
+
+		// Move the leftCharacter pointer towards the right
+		leftCharacter++
+
+		// Move the rightCharacter pointer towards the left
+		rightCharacter--
+	}
+}
