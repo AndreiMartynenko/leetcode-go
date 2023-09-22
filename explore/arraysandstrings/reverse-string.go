f55b41b3a95623b7ed419d93bccc84cde578c05f
AndrewLondon <andrewmart@mail.com>
2023-09-22 21:49:20 +0100

    Updated

--- explore/arraysandstrings/reverse-string.go
+++ explore/arraysandstrings/reverse-string.go
@@ -1,5 +1,31 @@
+/*
+Reverse string
+Write a function that reverses a string.
+The input string is given as an array of characters s.
+
+You must do this by modifying the input array in-place with O(1) extra memory.
+
+Example 1:
+
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+Example 2:
+
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+
+
+Constraints:
+
+1 <= s.length <= 105
+s[i] is a printable ascii character.
+*/
+
 package main
 
+import "fmt"
+
+/*
 func reverseString(s []byte) {
 	leftCharacter := 0
 	rightCharacter := len(s) - 1
@@ -15,7 +41,25 @@ func reverseString(s []byte) {
 	}
 }
 
+*/
+
+// func reverseString(s []byte) {
+func reverseString(s []byte) []byte {
+	var leftCharacter = 0
+	var rightCharacter = len(s) - 1
+
+	for leftCharacter < rightCharacter {
+		s[leftCharacter], s[rightCharacter] = s[rightCharacter], s[leftCharacter]
+
+		leftCharacter++
+		rightCharacter--
+	}
+	//fmt.Println(s)
+	return s
+
+}
 func main() {
 	var testWord = "Hello World!"
-	reverseString([]byte(testWord))
+	var newWord = reverseString([]byte(testWord))
+	fmt.Println(string(newWord))
 }
