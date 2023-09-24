48d8bdb69574160c141f881dac08f9e06083be2f
AndrewLondon <andrewmart@mail.com>
2023-09-24 23:57:29 +0100

    Updated

--- explore/arraysandstrings/subarray-product-less-than-k.go
+++ explore/arraysandstrings/subarray-product-less-than-k.go
@@ -26,7 +26,7 @@ func numSubarrayProductLessThanK(nums []int, k int) int {
 	if k <= 1 {
 		return 0
 	}
-	left, curr, ans := 0, 0, 0
+	ans, left, curr := 0, 0, 1
 	for right := 0; right < len(nums); right++ {
 		curr += nums[right]
 		for curr > k {
