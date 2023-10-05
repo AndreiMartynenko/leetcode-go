437f1ce89bf88768b985883fb1ac948e31ecf885
AndrewLondon <andrewmart@mail.com>
2023-10-05 20:07:27 +0100

    Updated

--- explore/arraysandstrings/k-radius-subarray-averages.go
+++ explore/arraysandstrings/k-radius-subarray-averages.go
@@ -56,6 +56,41 @@ elements before and after index 0.
 
 package main
 
-func getAverages(nums []int, k int) []int {
+// Approach 1: Prefix Sum
 
+func getAverages(nums []int, k int) []int {
+	// When a single element is considered then its average will be the number itself only.
+	if k == 0 {
+		return nums
+	}
+
+	n := len(nums)
+	averages := make([]int, n)
+	for i := range averages {
+		averages[i] = -1
+	}
+
+	// Any index will not have 'k' elements in its left and right.
+	if 2*k+1 > n {
+		return averages
+	}
+
+	// Generate 'prefix' array for 'nums'.
+	// 'prefix[i + 1]' will be sum of all elements of 'nums' from index '0' to 'i'.
+	prefix := make([]int64, n+1)
+	for i := 0; i < n; i++ {
+		prefix[i+1] = prefix[i] + int64(nums[i])
+	}
+
+	// We iterate only on those indices which have at least 'k' elements in their left and right.
+	// i.e. indices from 'k' to 'n - k'
+	for i := k; i < (n - k); i++ {
+		leftBound := i - k
+		rightBound := i + k
+		subArraySum := prefix[rightBound+1] - prefix[leftBound]
+		average := int(subArraySum / int64(2*k+1))
+		averages[i] = average
+	}
+
+	return averages
 }
