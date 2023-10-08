fb5e3639a0f7d62f84ca32b84cb2e6423d1614cd
AndrewLondon <andrewmart@mail.com>
2023-10-08 23:58:17 +0100

    Updated

--- top100liked/maximum-depth-of-binary-tree.go
+++ top100liked/maximum-depth-of-binary-tree.go
@@ -25,3 +25,18 @@ type TreeNode struct {
 	Left  *TreeNode
 	Right *TreeNode
 }
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	leftDepth := maxDepth(root.Left)
+	rightDepth := maxDepth(root.Right)
+
+	if leftDepth > rightDepth {
+		return leftDepth + 1
+	} else {
+		return rightDepth + 1
+	}
+}
