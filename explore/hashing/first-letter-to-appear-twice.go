69c527e728960986d46e04d6ca14a4ddc62bbf9c
AndrewLondon <andrewmart@mail.com>
2023-10-11 23:56:14 +0100

    Updated

---
+++ explore/hashing/first-letter-to-appear-twice.go
@@ -0,0 +1,7 @@
+/*
+Hashing
+Example 2: 2351. First Letter to Appear Twice
+
+Given a string s, return the first character to appear twice.
+It is guaranteed that the input will have a duplicate character.
+*/
