2b5cf90a7c0686949d6400d2c8eb50b2448983aa
AndrewLondon <andrewmart@mail.com>
2023-10-11 23:56:34 +0100

    Updated

--- explore/hashing/first-letter-to-appear-twice.go
+++ explore/hashing/first-letter-to-appear-twice.go
@@ -5,3 +5,9 @@ Example 2: 2351. First Letter to Appear Twice
 Given a string s, return the first character to appear twice.
 It is guaranteed that the input will have a duplicate character.
 */
+
+package main
+
+func main() {
+
+}
