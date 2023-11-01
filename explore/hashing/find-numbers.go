815ee82d354acd1d730b4f406402177c3128ddd1
AndrewLondon <andrewmart@mail.com>
2023-11-01 23:38:13 +0000

    Updated

---
+++ explore/hashing/find-numbers.go
@@ -0,0 +1,15 @@
+/*
+Hashing
+
+Example 3: Given an integer array nums,
+find all the numbers x in nums that
+satisfy the following: x + 1 is not in nums,
+and x - 1 is not in nums.
+
+*/
+
+package main
+
+func findNumbers(nums []int) []int {
+
+}
