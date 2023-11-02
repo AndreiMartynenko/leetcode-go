fb48148506a5da1fad02a6fac530096b389f48a3
AndrewLondon <andrewmart@mail.com>
2023-11-02 23:59:46 +0000

    Updated

--- explore/hashing/missing-number.go
+++ explore/hashing/missing-number.go
@@ -37,5 +37,12 @@ appear in nums.
 package main
 
 func missingNumber(nums []int) int {
+	n := len(nums)
+	expectedSum := n * (n + 1) / 2
+	actualSum := 0
+
+	for _, num := range nums {
+		actualSum += num
+	}
 
 }
