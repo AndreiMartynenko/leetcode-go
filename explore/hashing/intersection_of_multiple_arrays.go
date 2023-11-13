395d152577dcc7ea9917ec3990d5d9ea7e645738
AndrewLondon <andrewmart@mail.com>
2023-11-13 18:58:56 +0000

    Updated

--- explore/hashing/intersection_of_multiple_arrays.go
+++ explore/hashing/intersection_of_multiple_arrays.go
@@ -12,20 +12,22 @@ For example, given nums = [[3,1,2,4,5],[1,2,3,4],[3,4,5,6]],
 return [3, 4]. 3 and 4 are the only numbers that are in all arrays.
 */
 
-package main 
+package main
+
+import "sort"
 
 func intersect(nums [][]int) []int {
 	if len(nums) == 0 {
 		return []int{}
 	}
 
-		// Initialize the result with the numbers from the first array
-		result := make(map[int]int)
-		for _, num := range nums[0] {
-			result[num]++
-		}
+	// Initialize the result with the numbers from the first array
+	result := make(map[int]int)
+	for _, num := range nums[0] {
+		result[num]++
+	}
 
-			// Iterate through the rest of the arrays
+	// Iterate through the rest of the arrays
 	for i := 1; i < len(nums); i++ {
 		currentMap := make(map[int]int)
 
@@ -33,5 +35,25 @@ func intersect(nums [][]int) []int {
 		for _, num := range nums[i] {
 			currentMap[num]++
 		}
+		// Update the result map to only include numbers present in both arrays
+		for num, count := range result {
+			if currentCount, ok := currentMap[num]; ok {
+				// Choose the minimum count to account for duplicates
+				result[num] = min(count, currentCount)
+			} else {
+				// If the number is not present in the current array, remove it from the result
+				delete(result, num)
+			}
+		}
+	}
 
+	// Convert the result map to a sorted array
+	var intersection []int
+	for num := range result {
+		intersection = append(intersection, num)
+	}
 
+	// Sort the array before returning
+	sort.Ints(intersection)
+	return intersection
+}
