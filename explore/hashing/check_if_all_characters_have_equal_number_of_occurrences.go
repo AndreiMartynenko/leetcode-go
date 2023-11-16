8dc98f48882cb22def152b9bc83ec352fe41044c
AndrewLondon <andrewmart@mail.com>
2023-11-16 12:59:50 +0000

    Updated

---
+++ explore/hashing/check_if_all_characters_have_equal_number_of_occurrences.go
@@ -0,0 +1,36 @@
+/*
+HASHING
+Counting
+Example 3: 1941. Check if All Characters
+Have Equal Number of Occurrences
+
+Given a string s, determine if all
+characters have the same frequency.
+
+For example, given s = "abacbc", return true.
+All characters appear twice. Given s = "aaabb",
+return false. "a" appears 3 times, "b"
+appears 2 times. 3 != 2.
+
+Example 1:
+
+Input: s = "abacbc"
+Output: true
+Explanation: The characters that appear in s are
+'a', 'b', and 'c'. All characters occur 2 times in s.
+
+Example 2:
+
+Input: s = "aaabb"
+Output: false
+Explanation: The characters that
+appear in s are 'a' and 'b'.
+'a' occurs 3 times while 'b' occurs 2 times,
+which is not the same number of times.
+
+*/
+
+func areOccurrencesEqual(s string) bool {
+	count := 0
+
+}
