ce7df7191a489f788f82483245c79ca385baa3b2
AndrewLondon <andrewmart@mail.com>
2023-11-23 16:35:26 +0000

    Updated

--- explore/hashing/group_anagrams.go
+++ explore/hashing/group_anagrams.go
@@ -9,3 +9,21 @@ group the anagrams together.
 For example, given strs = ["eat","tea","tan","ate","nat","bat"],
 return [["bat"],["nat","tan"],["ate","eat","tea"]].
 */
+
+package main
+
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
+func groupAnagrams(strs []string) [][]string {
+	anagrams := make(map[string][]string)
+
+	for _, str := range strs {
+		sortedStr := sortString(str)
+
+
+
+
