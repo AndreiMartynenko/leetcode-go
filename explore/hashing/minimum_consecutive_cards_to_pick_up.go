2975523af0c1d75b45bf8c25c31c338e7e35d608
AndrewLondon <andrewmart@mail.com>
2023-11-24 15:48:52 +0000

    Updated

---
+++ explore/hashing/minimum_consecutive_cards_to_pick_up.go
@@ -0,0 +1,13 @@
+/*
+Example 2: 2260. Minimum Consecutive Cards to Pick Up
+
+Given an integer array cards, find the length of the
+shortest subarray that contains at least one duplicate.
+If the array has no duplicates, return -1.
+*/
+
+package main
+
+func minimumCardPickup(cards []int) int {
+
+}
