9910d3f856b65a0d2c79612db128a9c223cb0f68
AndrewLondon <andrewmart@mail.com>
2023-11-26 23:51:54 +0000

    Updated

---
+++ explore/hashing/max_sum_of_a_pair_with_equal_sum_of_digits.go
@@ -0,0 +1,18 @@
+/*
+HASHING
+
+Example 3: 2342. Max Sum of a Pair With Equal Sum of Digits
+
+Given an array of integers nums, find the maximum value of nums[i] + nums[j],
+where nums[i] and nums[j] have the same digit sum (the sum of their individual digits).
+Return -1 if there is no pair of numbers with the same digit sum.
+*/
+
+package main
+
+func maximumSum(nums []int) int {
+
+	digitSumMap := make(map[int][]int)
+	maxSum := -1
+
+}
