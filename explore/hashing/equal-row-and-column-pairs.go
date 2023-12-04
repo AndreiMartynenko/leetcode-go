7aa63350f1ee26e01e6924408a58aeed48bb98af
AndrewLondon <andrewmart@mail.com>
2023-12-04 00:04:50 +0000

    Updated

--- explore/hashing/equal-row-and-column-pairs.go
+++ explore/hashing/equal-row-and-column-pairs.go
@@ -9,21 +9,49 @@ we consider them as 1D arrays.
 
 package main
 
-func numEqualPairs(grid [][]int) int {
-	n := len(grid)
-	count := 0
+import (
+	"fmt"
+	"strings"
+)
 
-	for i := 0; i < n; i++ {
-		for j := 0; j < n; j++ {
-			for k := 0; k < n; k++ {
-				for l := 0; l < n; l++ {
-					if grid[i][j] == grid[k][l] && i != k && j != l {
-						count++
-					}
-				}
-			}
-		}
+func convertToKey(arr []int) string {
+	var key strings.Builder
+
+	for _, num := range arr {
+		key.WriteString(fmt.Sprintf("%d,", num))
 	}
-	return count / 2 // Each pair is counted twice (R, C) and (C, R)
 
+	return key.String()
+}
+
+func equalPairs(grid [][]int) int {
+	dic := make(map[string]int)
+
+	// Count occurrences for each row
+	for _, arr := range grid {
+		key := convertToKey(arr)
+		dic[key]++
+	}
+
+	dic2 := make(map[string]int)
+
+	// Count occurrences for each column
+	for col := 0; col < len(grid[0]); col++ {
+		var currentCol []int
+		for row := 0; row < len(grid); row++ {
+			currentCol = append(currentCol, grid[row][col])
+		}
+
+		key := convertToKey(currentCol)
+		dic2[key]++
+	}
+
+	ans := 0
+
+	// Calculate the number of equal pairs
+	for key, val := range dic {
+		ans += val * dic2[key]
+	}
+
+	return ans
 }
