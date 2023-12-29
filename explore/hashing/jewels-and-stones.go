d6a7ee65a3ff59375f110562ea14c17cb55c6f49
AndrewLondon <andrewmart@mail.com>
2023-12-29 16:05:13 +0000

    Updated

--- explore/hashing/jewels-and-stones.go
+++ explore/hashing/jewels-and-stones.go
@@ -1,6 +1,12 @@
 /*
 
-You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+HASHING
+
+Jewels and Stones
+
+You're given strings jewels representing the types of stones that are jewels,
+and stones representing the stones you have. Each character in stones is a type
+of stone you have. You want to know how many of the stones you have are also jewels.
 
 Letters are case sensitive, so "a" is considered a different type of stone from "A".
 
@@ -8,11 +14,11 @@ Example 1:
 
 Input: jewels = "aA", stones = "aAAbbbb"
 Output: 3
+
 Example 2:
 
 Input: jewels = "z", stones = "ZZ"
 Output: 0
- 
 
 Constraints:
 
@@ -22,5 +28,10 @@ All the characters of jewels are unique.
 
 */
 
-
 package main
+
+func numJewelsInStones(jewels string, stones string) int {
+
+	return 0
+
+}
