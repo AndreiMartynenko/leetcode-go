4336dd73cd805742b2fcee7a906192eabeeffcdf
AndrewLondon <andrewmart@mail.com>
2024-01-04 22:45:23 +0000

    Update

--- explore/hashing/longest-substring-without-repeating-characters.go
+++ explore/hashing/longest-substring-without-repeating-characters.go
@@ -1,13 +1,14 @@
 package main
 
+// Sliding Window approach
 func lengthOfLongestSubstring(s string) int {
 	charIndex := make(map[byte]int)
 	maxLength := 0
 	start := 0
 
 	for end := 0; end < len(s); end++ {
-		if charIndex[s[end]] >= start {
-			start = charIndex[s[end]] + 1
+		if idx, found := charIndex[s[end]]; found && idx >= start {
+			start = idx + 1
 		}
 		charIndex[s[end]] = end
 		currentLength := end - start + 1
