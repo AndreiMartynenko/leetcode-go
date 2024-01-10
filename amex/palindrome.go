23555e870e1c94700bcf57931167a8f91e6e5488
AndrewLondon <andrewmart@mail.com>
2024-01-10 22:11:25 +0000

    AMEX updated

--- amex/palindrome.go
+++ amex/palindrome.go
@@ -1,1 +1,34 @@
 package main
+
+/*
+A palindrome is a ward which reads the same
+backward as forward. Some examples of palindromes
+are “kayak”, “radar”, “mom”
+
+Write a function
+func Solution(N int, K int) string
+
+that given two integers N and K,
+returns a palindrome of length N which
+consists of K distinct lowercase letters (a-z).
+
+Examples
+given N = 5, K= 2  your function may return “abbba”.  there are many other possibilities for example it could also return “zdzdz”.  on the other hand “aaaaa”  is an incorrect answer is it contains only one distinct letter.
+given N= 8, K = 3, Your function may return “ppsccspp”.
+given N = 3, K = 2, Your function may return “opo”.
+
+Assume that
+
+N  Is an integer within the range [1..200];
+K  is an integer within the range [1..26];
+
+creation of the required palindrome is always possible.
+in use solution focus on correctness the performance
+of your solution will not be the focus of the assessment.
+
+*/
+
+func Solution(N int, K int) string {
+
+	return string(solution)
+}
