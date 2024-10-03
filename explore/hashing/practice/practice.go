d2d330c0715df8a28a9767c3b16df42f9a1e0fe1
AndrewLondon <andrewmart@mail.com>
2024-10-03 13:37:35 +0100

    updated

--- explore/hashing/practice/practice.go
+++ explore/hashing/practice/practice.go
@@ -4,39 +4,36 @@ import "fmt"
 
 //hashMap := make(map[int]int)
 
-hashMap := map[int]int{
-	1: 2,
-	5: 3,
-	7: 2,
-}
-
-//check if the key exists
-
-value, ok := hashMap[1]
-_, ok := hashMap[9]
-
-//Accessing the value of a key
-value := hashMap[5]
-
-//Adding or updating a new key-value pair:
-hashMap[5] = 6
-
-//If the key does not exist, it will be added to the map.
-hashMap[9] = 15
-
-//Deleting a key-value pair:
-delete(hashMap, 9)
-
-//Get size of the map
-
-size := len(hashMap)
-
 //Iterating over a map
 
-for key, value := range hashMap {
-	fmt.Println(key, value)
-}
-
 func main() {
-	fmt.Println("Hello, playground")
+
+	hashMap := map[int]int{
+		1: 2,
+		5: 3,
+		7: 2,
+	}
+
+	//check if the key exists
+	value, ok := hashMap[1]
+	_, ok := hashMap[9]
+
+	//Accessing the value of a key
+	value := hashMap[5]
+
+	//Adding or updating a new key-value pair:
+	hashMap[5] = 6
+
+	//If the key does not exist, it will be added to the map.
+	hashMap[9] = 15
+
+	//Deleting a key-value pair:
+	delete(hashMap, 9)
+
+	//Get size of the map
+	size := len(hashMap)
+
+	for key, value := range hashMap {
+		fmt.Println(key, value)
+	}
 }
