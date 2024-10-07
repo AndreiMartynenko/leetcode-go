e0a1c70696b07466e9c8403eaedea14fc58b70f6
AndrewLondon <andrewmart@mail.com>
2024-10-07 13:47:12 +0100

    updated

--- explore/hashing/check_if_pangram.go
+++ explore/hashing/check_if_pangram.go
@@ -1,11 +1,21 @@
 package main
 
+import "fmt"
+
 func checkIfPangram(sentence string) bool {
 	seen := make(map[rune]bool)
 
 	for _, char := range sentence {
-		seen[char] = true
-	}
+		if 'a' <= char && char <= 'z' {
+			seen[char] = true
+		}
 
-	return len(seen) == 26
+		return len(seen) == 26
+	}
+}
+
+func main() {
+	sentence := "thequickbrownfoxjumpsoverthelazydog"
+	result := checkIfPangram(sentence)
+	fmt.Println(result)
 }
