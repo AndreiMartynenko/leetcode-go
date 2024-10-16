bb3bc52adaf21825e6cda9dd4f85c855f03b9992
AndrewLondon <andrewmart@mail.com>
2024-10-16 20:14:12 +0100

    updated

--- explore/hashing/practice/practice.go
+++ explore/hashing/practice/practice.go
@@ -48,6 +48,8 @@ For example, if `s = "eceba"` and `k = 2`, the answer is 3. The longest substrin
 
 //Check if the key exists in the map
 //value, ok := hashMap[key]
+//if ok {
+//	fmt.Println(value)
 
 //func lengthOfLongestSubstring(s string, k int) {
 //	hashMap := make(map[rune]int)
